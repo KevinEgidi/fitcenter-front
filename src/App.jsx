c11d95fe66703476688467aab456ab954b1be43a
GenaBercini <bercinigenaro@gmail.com>
2025-07-23 11:57:56 -0300

    Changes to Chakra

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,14 @@ import './App.css'
 import Landing from './pages/Landing'
 import Navbar from './components/NavBar'
 import Footer from './components/Footer'
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './utils/theme';
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Navbar />
-        <Landing />
-        <Footer />
-      </ThemeProvider>
+      <Navbar />
+      <Landing />
+      <Footer />
     </>
   )
 }
-
 export default App
