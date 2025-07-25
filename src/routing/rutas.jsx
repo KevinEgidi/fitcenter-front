d3a69bc01900b261e92972831ff5245bae7ccf8b
Nicolás Piccoli <nicopiccoli07@gmail.com>
2025-07-24 23:31:00 -0300

    boilerplate cart

--- src/routing/rutas.jsx
+++ src/routing/rutas.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import {Route} from 'react-router-dom';
 import Landing from "../pages/Landing";
-import NavBar from "../components/Layaut/NavBar";
-import Footer from "../components/Layaut/Footer";
 import Cart from "../pages/cart";
 
-
 export const rutas = [
-    <>
-        <Route path="/" element={<Landing/>} />
-        <Route path="/" element={<Landing/>} />
+        <Route path="/" element={<Landing/>} />,
+        <Route path="/" element={<Landing/>} />,
         <Route path="/cart" element={<Cart/>}/>
-    </>
+
 ]
